abb70e06491c6dbbd1705090483471a85e9eeec6
Vladimir Savostyanenko <>
2025-07-24 00:42:45 +0300

    Update manifest start_url

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1953113119"
+    serviceWorkerVersion: "4200326036"
   }
 });
